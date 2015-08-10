acf81b010abd11b7ef253875188fe1afac5ec82c
Balathasan Giritharan <balathasan.giritharan@monsanto.com>
2015-08-09 21:40:04 -0500

    Verbose pascal function.

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -5,24 +5,38 @@ import common._
 object Main {
   def main(args: Array[String]) {
 
+
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
-        print(pascal(col, row) + "### ")
+        print(pascal(col, row) + "  ")
       println()
     }
+    //println(pascal(0,0))
+    //println("\n1,1",pascal(1,1))
+    //println(pascal(0,0))
 
   }
 
   /**  Exercise 1 */
   def pascal ( col:Int, row:Int ):Int =
   {
-    if ((row == 0) || (col==0))                   { 1 }
-    else if ((row==1) && ((col==0) || (col==1)))  { 1 }
-    else if (row<=col)                            { 1 }
+    if ((row == 0)  || (col==0))  {
+      //def myStr = "ROW-0 [%d,%d]".format( row, col);  println (myStr)
+      1
+    }
+    else if ((row==1) && ((col==0) || (col==1)))
+    {
+      //def myStr = "ROW-1 [%d,%d]".format( row, col);println (myStr)
+      1
+    }
+    else if (row<=col)
+    {
+      //def myStr = "[%d,%d]".format( row, col)  ;print (myStr)
+      1
+    }
     else {
-      def myStr = "[%d,%d]".format( row, col)
-      print (myStr)
+      //def myStr = "[%d,%d]".format( row, col); print (myStr)
       pascal(col-1,row-1) + pascal (col,row-1)
     }
   }
